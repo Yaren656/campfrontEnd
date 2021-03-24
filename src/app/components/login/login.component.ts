7aa18d0516f4d59245dbac885153d33f4113182c
Yaren Yuvka <yarenyuvka@gmail.com>
2021-03-24 21:39:43 +0300

    24.03.2021

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(private fromBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.fromBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
+}
