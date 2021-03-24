37984256bc5244776d309f4e5ae913ef0beac9cb
Yaren Yuvka <yarenyuvka@gmail.com>
2021-03-24 23:37:12 +0300

    24.03.2021

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -25,6 +25,6 @@ export class ProductService {
   }
 
   add(product:Product):Observable<ResponseModel>{
-    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add", product)
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product)
   }
 }
